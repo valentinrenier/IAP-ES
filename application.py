f27e4338ec11c423f55f4e55e002fac2a49ade62
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-11-04 22:34:51 +0000

    modif on the app

--- application.py
+++ application.py
@@ -2,14 +2,14 @@ from flask import Flask
 from secret import FLASK_SECRET
 from apis import api
 
-application = Flask(__name__)
-application.secret_key = FLASK_SECRET
 
 def create_app():
     application = Flask(__name__)
     api.init_app(application)
     application.secret_key=FLASK_SECRET
     return application
+
+application=create_app()
 
 if __name__ == '__main__' :
     application.run(host='0.0.0.0', port='80', debug=True)
