342cb8502d96c90cb79135f224e8b2aca23fd976
root <root@ip-10-0-7-241.ec2.internal>
2024-11-05 00:56:06 +0000

    add

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restx import Api, Namespace
 
-from .api import api as api
+from .api import api as api2
 from .ui import api as ui
 from .auth import api as auth
 
@@ -9,6 +9,6 @@ blueprint = Blueprint('endpoints', __name__)
 
 api = Api(version="1.0", title="IAP API", description="API for the IAP", prefix="/")
 
-api.add_namespace(api)
+api.add_namespace(api2)
 api.add_namespace(ui)
 api.add_namespace(auth)
--- apis/ui.py
+++ apis/ui.py
@@ -1,8 +1,9 @@
-from flask import render_template, session, make_response
+from flask import render_template, make_response
+from flask import session as sess
 from flask_restx import Namespace, Resource
-from data.models import Task
+from data.models.Task import Task
 from apis.auth import check_token
-from data.db_session import Session
+from sqlalchemy.orm import Session
 from data.db_engine import engine
 
 api=Namespace("ui",path="/ui",description="UI")
@@ -11,9 +12,9 @@ api=Namespace("ui",path="/ui",description="UI")
 class index(Resource):
     def get(self):
         if check_token():
-            username = session['cognito:username']
-            with Session(engine) as sess :
-                tasks = sess.query(Task).filter(Task.user == username).order_by(Task.created_at.desc()).all()
+            username = sess['cognito:username']
+            with Session(engine) as session :
+                tasks = session.query(Task).filter(Task.user == username).order_by(Task.created_at.desc()).all()
                 return make_response(
                     render_template('index.html', tasks = tasks, user=True),
                     200,
