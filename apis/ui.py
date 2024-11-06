500ca29fe2ee19a2c03fd3267e5364ef8aaba9d8
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-11-06 14:20:08 +0000

    adding filters

--- apis/ui.py
+++ apis/ui.py
@@ -1,11 +1,7 @@
 from flask import render_template, make_response, request
-from flask import session as flask_session
 from flask_restx import Namespace, Resource
-from data.models.Task import Task
 from apis.auth import check_token
-from sqlalchemy.orm import Session
-from data.db_engine import engine
-from sqlalchemy import case
+from services.db_services import get_all_tasks
 
 api=Namespace("ui",path="/ui",description="UI")
 
@@ -13,36 +9,14 @@ api=Namespace("ui",path="/ui",description="UI")
 class index(Resource):
     def get(self):
         if check_token():
-
-            with Session(engine) as session :
-                priority_order = case(
-                    (Task.priority == 'Low', 1),
-                    (Task.priority == 'Medium', 2),
-                    (Task.priority == 'High', 3),
-                    else_=4  # Valeur par défaut si la priorité n'est pas définie
-                )
-                order_by = request.args.get('order_by', 'created_at_desc')  # Valeur par défaut
-                query = session.query(Task).filter(Task.user == flask_session['cognito:username'])
-                # Appliquer l'ordre de tri en fonction de la sélection
-                if order_by == "created_at_asc":
-                    query = query.order_by(Task.created_at.asc())
-                elif order_by == "created_at_desc":
-                    query = query.order_by(Task.created_at.desc())
-                elif order_by == "priority_asc":
-                    query = query.order_by(priority_order.asc())
-                elif order_by == "priority_desc":
-                    query = query.order_by(priority_order.desc())
-                elif order_by == "deadline_asc":
-                    query = query.order_by(Task.deadline.asc())
-                elif order_by == "deadline_desc":
-                    query = query.order_by(Task.deadline.desc())
-
-                tasks = query.all()
-                return make_response(
-                    render_template('index.html', tasks = tasks, user=True),
-                    200,
-                    {'Content-Type': 'text/html'}
-                )
+            order_by = request.args.get('order_by', 'created_at_desc')
+            filters = request.args.getlist('filters[]')
+            tasks = get_all_tasks(order_by, filters)
+            return make_response(
+                render_template('index.html', tasks = tasks, user=True),
+                200,
+                {'Content-Type': 'text/html'}
+            )
         else :
             return make_response(
                 render_template('index.html', tasks = None, user=None),
--- services/db_services.py
+++ services/db_services.py
@@ -5,11 +5,11 @@ from data.db_engine import engine
 from flask import session as flask_session
     
 from datetime import datetime, timedelta
-from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from sqlalchemy.orm.exc import NoResultFound
 
-from flask import flash
+from flask import flash, request
+from sqlalchemy import case
 
 def add_task(title, description, priority, deadline):
     with Session(engine) as session :
@@ -58,3 +58,47 @@ def modify_task(task_id, data):
                 return True 
     except :
         return False
+    
+def get_all_tasks(order_by, filters):
+    priority_order = case(
+        (Task.priority == 'Low', 1),
+        (Task.priority == 'Medium', 2),
+        (Task.priority == 'High', 3),
+        else_=4
+    )
+    
+    order_by_options = {
+        "created_at_asc": Task.created_at.asc(),
+        "created_at_desc": Task.created_at.desc(),
+        "priority_asc": priority_order.asc(),
+        "priority_desc": priority_order.desc(),
+        "deadline_asc": Task.deadline.asc(),
+        "deadline_desc": Task.deadline.desc(),
+        "completed_asc": Task.completed.asc(),
+        "completed_desc": Task.completed.desc(),
+    }
+
+    order_by_method = order_by_options.get(order_by)
+    
+    show_completed = 'completed' in filters
+    show_not_completed = 'not-completed' in filters
+    show_priorities = [f.split('-')[0] for f in filters if 'priority' in f]
+
+    with Session(engine) as session :
+        query = session.query(Task).filter(Task.user == flask_session['cognito:username'])
+        
+        #Filtering
+        if show_completed and show_not_completed :
+            pass
+        elif show_completed :
+            query = query.filter(Task.completed == True)
+        elif show_not_completed :
+            query = query.filter(Task.completed == False)
+        if show_priorities :
+            query = query.filter(Task.priority.in_(show_priorities))
+            
+        if order_by_method:
+            query = query.order_by(order_by_method)
+        tasks = query.all()
+        return tasks
+
